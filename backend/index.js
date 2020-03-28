1055b31e47af73ca92c03d138a5e6ee3266a1d92
Tiago Lima <tiago.lima@osf.digital>
2020-03-28 16:08:39 -0300

    Implemented Back-End.

--- backend/index.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-
-const app = express();
-
-app.get("/", (req, res) => {
-  return res.json({ evento: "OmniStack 11.0", aluno: "Tiago Lima" });
-});
-
-app.listen(3333);
