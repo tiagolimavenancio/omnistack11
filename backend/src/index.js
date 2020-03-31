51ef006b358ba49b81280ea2bb0b16687d8ea492
Tiago Lima <tiago.lima@osf.digital>
2020-03-31 10:34:49 -0300

    Implemented Front-End.

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,11 +3,7 @@ const cors = require("cors");
 const routes = require("./routes");
 const app = express();
 
-app.use(
-  cors({
-    origin: "http://meuapp.com"
-  })
-);
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
