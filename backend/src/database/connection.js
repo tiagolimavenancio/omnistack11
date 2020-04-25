8718cc1be8637c1e4e45c3e65c1f652eaf3cec2f
Tiago Lima <tiago.lima@osf.digital>
2020-04-25 13:31:22 -0300

    Made the validations. Made the Tests with Jest.

--- backend/src/database/connection.js
+++ backend/src/database/connection.js
@@ -1,4 +1,11 @@
 const knex = require("knex");
-const knexfile = require("../../knexfile");
+const configuration = require("../../knexfile");
 
-module.exports = knex(knexfile.development);
+const config =
+  process.env.NODE_ENV === "test"
+    ? configuration.test
+    : configuration.development;
+
+const connection = knex(config);
+
+module.exports = connection;
